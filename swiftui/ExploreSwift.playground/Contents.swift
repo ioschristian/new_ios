170b05d684f5edca1b3f11545a9fec095ba5bab6
Christian Tisby <christian.d.tisby@gmail.com>
2022-12-07 22:08:54 -0500

    completed storm viewer adding guess the flag start

---
+++ swiftui/ExploreSwift.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let likeHummus = "likes Hummus"
+print(greeting)
+
+greeting = "Likes Hummus"
+
+print(greeting)
+
+greeting = "Ron " + greeting
+
+var name = "Ron"
+
+greeting = name + " " + likeHummus
+
