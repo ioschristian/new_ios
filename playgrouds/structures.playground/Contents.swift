7e2bda886a79a69121fdd07553a9cf1ea3862338
Christian Tisby <christian.d.tisby@gmail.com>
2022-12-14 23:03:02 -0500

    adding completed swifty words project

--- playgrouds/structures.playground/Contents.swift
+++ playgrouds/structures.playground/Contents.swift
@@ -1,3 +1,52 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Person {
+    var name: String
+    func sayHello() {
+        print("Hello, there! My name is \(name)")
+    }
+}
+
+let firstPerson = Person(name: "Jasmine")
+print(firstPerson.name)
+
+
+let person = Person(name: "Jasmine")
+person.sayHello()
+
+struct Odometer {
+    var count: Int = 0
+}
+
+
+let odometer = Odometer()
+print(odometer.count)
+
+
+struct Women {
+    var name: String = "Ebony"
+}
+
+let women = Women()
+print(women.name)
+
+
+struct BankAccount {
+    var accountNumber: Int
+    var balance: Double = 0
+}
+
+var newAccount = BankAccount(accountNumber: 123)
+var transferredAccount = BankAccount(accountNumber: 456, balance: 1200)
+var sharedAccount = BankAccount(accountNumber: 434)
+
+
+//struct Temperature {
+//    var celsius: Double
+//}
+//
+//
+////let temperature = Temperature(celsius: 30.0)
+//let fahrenheitValue = 98.6
+//let celsiusValue = (fahrenheitValue - 32) / 1.8
+//let temperature = Temperature(celsius: celsiusValue)
