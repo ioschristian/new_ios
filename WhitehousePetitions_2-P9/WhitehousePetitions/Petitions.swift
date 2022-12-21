dc7de7bbc78c252c0cf8edc25bd96e4695010dcc
Christian Tisby <christian.d.tisby@gmail.com>
2022-12-21 12:42:02 -0500

    added completed project 9 whitehouse petitions

---
+++ WhitehousePetitions_2-P9/WhitehousePetitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhitehousePetitions
+//
+//  Created by Christian Tisby on 12/13/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
